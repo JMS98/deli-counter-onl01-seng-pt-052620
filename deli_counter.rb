d5a16104f5f7facd265f2a7db60dda9352a23649
Jonathan Snipe <jon.mike919@gmail.com>
2020-06-07 23:51:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,7 @@
-# Write your code here.
+require 'pry'
+
+def line(line_array)
+  if line_array.size == 0 
+    puts "The line is currently empty!"
+  else
+    name_array = line_array.map_with_index
